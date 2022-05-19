02d1d3106b440bafb055008fd940fd2e4ed553c0
mukesh2305 <yogahindu1999@gmail.com>
2022-05-19 19:34:12 +0530

    first

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middleware/user.middleware").default;
+const protect = require("../middleware/user.middleware");
 const userController = require("../controllers/user.controllers");
 
 
